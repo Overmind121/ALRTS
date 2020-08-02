238b74fdcbd6a5972e52d80116033df6bcec2c96
Luke-Desktop <lukekim1029@gmail.com>
2020-08-01 22:45:39 -0700

    Updating

--- ALRT_app.py
+++ ALRT_app.py
@@ -1,5 +1,124 @@
-from data_fetch import FetchData as fd
 import kivy
-stock = input("what stock?")
-fetched_dat = fd(stock)
-print(fetched_dat.get_macd())
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from data_fetch import FetchData as fd
+from kivymd.uix.textfield import MDTextField
+from kivy.lang import Builder
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+
+stock_request = ""
+stock = fd("AAPL")
+focus = False
+
+screen_helper = """
+ScreenManager:
+    Main:
+    Info:
+    
+<Main>
+    name:'main'
+    stock_request_obj: stock_request_obj
+    MDTextField:
+        id: stock_request_obj
+        hint_text: "Enter a valid stock"
+        mode: "rectangle"
+        helper_text: "Ex: AAPL, aapl, AMZN, amzn"
+        helper_text_mode: "on_focus"
+        pos_hint: {'center_x':0.5,'center_y':0.5}
+        size_hint_x: None
+        width: 300
+        multiline:False
+        on_text_validate: root.manager.current = 'info'
+        on_text_validate: root.display_stock()
+        
+<Info>
+    name:'info'
+    FloatLayout:
+        GridLayout:
+            rows:1
+            pos_hint: {"top": 1, "left":1}
+            size_hint: 1, .1
+            
+            canvas:
+                Color:
+                    rgba: app.theme_cls.primary_color
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+                
+            MDLabel:
+                text: "MACD"
+                font_size: 50
+                font_style: "H6"
+                halign: "center"
+                
+            MDLabel:
+                text: root.labelText
+                font_size: 50
+                font_style: "H6"
+                halign: "center"
+            
+            MDLabel:
+                text: "RSI"
+                font_size: 50
+                font_style: "H6"
+                halign: "center"
+            
+        Live:
+           
+"""
+
+class Live(Label):
+    def __init__(self, **kwargs):
+        super(Live, self).__init__(**kwargs)
+        self.text = "none"
+        Clock.schedule_interval(self.update,1)
+
+    def update(self, *args):
+        self.text = str(stock.get_price())
+
+class Main(Screen):
+    global stock_request, stock
+    stock_request_obj = ObjectProperty(None)
+
+    def display_stock(self):
+        stock = fd(str(self.stock_request_obj.text))
+        stock_request = str(self.stock_request_obj.text).upper()
+        #print("Your stock is " + str(self.stock_request_obj.text))
+        self.manager.get_screen('info').labelText = stock_request
+        focus = True
+        return stock, stock_request
+
+class Info(Screen):
+    global stock, stock_request, focus
+    labelText = StringProperty('Stock')
+    MACD_label = StringProperty('none')
+    RSI_label = StringProperty('none')
+    Price_label = StringProperty('none')
+
+
+
+
+
+
+sm = ScreenManager()
+sm.add_widget(Main(name='main'))
+sm.add_widget(Info(name='info'))
+
+class ALRTApp(MDApp):
+    def build(self):
+        screen = Builder.load_string(screen_helper)
+        return screen
+
+
+
+if __name__ == "__main__":
+    ALRTApp().run()
--- data_fetch.py
+++ data_fetch.py
@@ -1,34 +1,16 @@
-from pandas_datareader import data as web
-from datetime import date, timedelta
+import pandas as pd
+import pandas_datareader.data as web
 from stockstats import StockDataFrame as Sdf
-
-
-# while True:
-#     dat = web.get_data_yahoo(input)
-#
-#     stockstats_df = Sdf.retype(dat)
-#     macd = stockstats_df['macd']
-#     macd_signal = stockstats_df['macds']
-#     rsi = stockstats_df['rsi_14']
-#
-#     print("RSI " + str(rsi.iloc[-1]))
-#     print("MACD " + str(macd.iloc[-1]))
-#     print("MACD_SIGNAL " + str(macd_signal.iloc[-1]))
-
+from yahoo_fin import stock_info as si
 class FetchData:
 
     def __init__(self, stock):
-        data = web.get_data_yahoo(stock)
-        self.stock = stock
-        self.num_days = 0
-        self.start_date = 0
-        self.end_date = 0
-        self.stock_dat = Sdf.retype(data)
-
-    def get_history(self, days):
-        self.num_days = days
-        self.start_date = (date.today() - timedelta(days=days)).isoformat()
-        self.end_date = date.today().isoformat()
+        if stock == "none":
+            data = 0
+        else:
+            data = web.get_data_yahoo(stock)
+            self.stock = stock
+            self.stock_dat = Sdf.retype(data)
 
     def get_macd(self):
         return self.stock_dat['macd'].iloc[-1]
@@ -39,6 +21,8 @@ class FetchData:
     def get_rsi(self):
         return self.stock_dat['rsi_14'].iloc[-1]
 
+    def get_price(self):
+        return si.get_live_price(self.stock)
 if __name__ == "__main__":
     stck = input("What Stock are you interested in?")
     fetched_dat = FetchData(stck)
